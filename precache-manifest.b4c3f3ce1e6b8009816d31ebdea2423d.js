8224b494220e0db02cfbb60d4d7ffc356adb996f
mleichty <mleichty@iu.edu>
2019-09-02 13:25:04 -0400

    Updates

---
+++ precache-manifest.b4c3f3ce1e6b8009816d31ebdea2423d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97164cbdcc85eb304566e8ad5128f5cd",
+    "url": "/react_monster/index.html"
+  },
+  {
+    "revision": "f2dd89e6fedcf85e5560",
+    "url": "/react_monster/static/css/main.b3381017.chunk.css"
+  },
+  {
+    "revision": "1fae22f8fdb3d7e21913",
+    "url": "/react_monster/static/js/2.3c2defdb.chunk.js"
+  },
+  {
+    "revision": "f2dd89e6fedcf85e5560",
+    "url": "/react_monster/static/js/main.672a3616.chunk.js"
+  },
+  {
+    "revision": "89e0721744d85f95acca",
+    "url": "/react_monster/static/js/runtime~main.0b7ec1a0.js"
+  },
+  {
+    "revision": "987bd5a2dd96954cc6ab42162a5212e1",
+    "url": "/react_monster/static/media/leftArrow.987bd5a2.png"
+  },
+  {
+    "revision": "5122012765f69cf42d78b696ffabdfa3",
+    "url": "/react_monster/static/media/monster1-1.51220127.png"
+  },
+  {
+    "revision": "bc3e480635653ca02dd25609e479a9a3",
+    "url": "/react_monster/static/media/monster1-2.bc3e4806.png"
+  },
+  {
+    "revision": "313cfb4a43a46702cb97bf8cb70e54a8",
+    "url": "/react_monster/static/media/monster1-3.313cfb4a.png"
+  },
+  {
+    "revision": "1d9b5ccf2f14c4c5dd602794b2d6bc3d",
+    "url": "/react_monster/static/media/monster2-1.1d9b5ccf.png"
+  },
+  {
+    "revision": "fdd790fb37a20fec9bb4518294809444",
+    "url": "/react_monster/static/media/monster2-2.fdd790fb.png"
+  },
+  {
+    "revision": "2ead22c82b441afa50a970a41ba7f427",
+    "url": "/react_monster/static/media/monster2-3.2ead22c8.png"
+  },
+  {
+    "revision": "7b8b501cf220019858a9dad989f702e4",
+    "url": "/react_monster/static/media/monster3-1.7b8b501c.png"
+  },
+  {
+    "revision": "172c4a03279e5c9b21700a1bb9a2823a",
+    "url": "/react_monster/static/media/monster3-2.172c4a03.png"
+  },
+  {
+    "revision": "9667e1591c4c76c37e221f7ee09d8d0f",
+    "url": "/react_monster/static/media/monster3-3.9667e159.png"
+  },
+  {
+    "revision": "f65d370f822bf47bb41c7eab9a613bf2",
+    "url": "/react_monster/static/media/rightArrow.f65d370f.png"
+  }
+]);
